697f57b1bad291397439e1520cd8adbd7ec222ab
charkonem <jacobdalaputa@icloud.com>
2019-05-17 16:20:18 +0800

    Site updated: 2019-05-17 16:20:16

--- dist/music.js
+++ dist/music.js
@@ -1,17 +1,27 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
+    lrcType: 3,
     audio: [{
-        name: '111',
-        artist: '222',
-        url: '/music/333.mp3',
-        cover: '/music/000.jpg',
-        theme: '#ebd0c2'
+        name: 'Up Is Down',
+        artist: 'Hans Zimmer',
+        url: '/music/Up Is Down.mp3',
+        cover: '/music/up is down.jpg',
+        theme: '#808080'
     },
- {
-            name: '222',
-            artist: '333',
-            url: '/music/Hans Zimmer-Up Is Down.wav',
-            cover: '/music/000.jpg',
-        }
+    {
+        name: 'Red Battle',
+        artist: '高橋李依;豊崎愛生',
+        url: '/music/Red Battle.mp3',
+        cover: '/music/Red Battle.jpg',
+        lrc: '/music/Red Battle-高橋李依;豊崎愛生.lrc',
+        theme: '#00BFFF'
+    },
+    {
+        name: '笑傲江湖曲',
+        artist: '胡伟立',
+        url: '/music/笑傲江湖曲.mp3',
+        cover: '/music/笑傲江湖曲.jpg',
+        theme: '#FF7F50'
+    }
     ]
 });
