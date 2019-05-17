150e024bedce9027cf510baa7eef990b667db981
charkonem <jacobdalaputa@icloud.com>
2019-05-17 23:00:06 +0800

    Site updated: 2019-05-17 23:00:04

---
+++ dist/m去而复返.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: 'First Sight',
+        artist: 'Junk',
+        url: '/music/Junk-First Sight.wav',
+        cover: '/music/Junk-First Sight.jpg',
+        theme: '#BEE7E9'
+    }
+    ]
+});
