115141448e2f06b17af1a750a395220cdc85f1fc
charkonem <jacobdalaputa@icloud.com>
2019-05-31 23:07:47 +0800

    Site updated: 2019-05-31 23:07:46

---
+++ dist/music collection 1/trine.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: 'Searock Castle',
+        artist: 'Ari Pulkkinen',
+        url: '/music/music collection 1/Ari Pulkkinen-Searock Castle.m4a',
+        cover: '/music/music collection 1/Ari Pulkkinen-Searock Castle.jpg',
+        theme: '#DAA520'
+    }
+    ]
+});
